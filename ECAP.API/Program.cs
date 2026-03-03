77b0cb943b61c5854be7490a6477171bec98020b
Muhammad Yusuf Arandy <134345500+RandyYusuf@users.noreply.github.com>
2026-03-03 09:10:42 +0000

    register exception middleware and swagger in program pipeline

--- ECAP.API/Program.cs
+++ ECAP.API/Program.cs
@@ -1,3 +1,5 @@
+using ECAP.API.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -7,11 +9,16 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // HARUS PALING ATAS SETELAH BUILD
+app.UseMiddleware<GlobalExceptionMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty; // Ini membuat swagger jadi halaman utama
+    });
 }
 
 app.UseAuthorization();
